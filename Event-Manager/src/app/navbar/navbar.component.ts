9c2b9cde0562759d9ef353ae01fece82b27f1b4b
Ahad0926 <ahadabdu0926@gmail.com>
2024-11-26 22:03:37 -0500

    Made navbar dynamic based on login

--- Event-Manager/src/app/navbar/navbar.component.ts
+++ Event-Manager/src/app/navbar/navbar.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.userService.isLoggedIn$.subscribe((loggedIn) => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
+
+  logout(): void {
+    this.userService.logout();
+    this.router.navigate(['']);
+  }
 }
